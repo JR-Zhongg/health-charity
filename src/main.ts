d782b1d662ac21f7b9c9df931d333733d3f332e1
JR-Zhongg <jzho0171@student.monash.edu>
2025-08-12 00:03:47 +0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,15 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import './styles/tokens.css'
 
-// ✅ 正确路径（修复）
-import 'primevue/resources/themes/lara-light-blue.css'
+import 'primevue/resources/themes/lara-light-blue.css' 
 import 'primevue/resources/primevue.min.css'
 import 'primeicons/primeicons.css'
 
+
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+
+import PrimeVue from 'primevue/config'
 
 import App from './App.vue'
 import router from './router'
@@ -18,5 +20,6 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(PrimeVue) // ✅ 挂载 PrimeVue
 
 app.mount('#app')
