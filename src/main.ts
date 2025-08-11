fdfa2ab190d7536ec43d24139eb460c752b27673
JR-Zhongg <jzho0171@student.monash.edu>
2025-08-12 01:11:42 +0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,16 +3,13 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import './styles/tokens.css'
 
-import 'primevue/resources/themes/lara-light-blue.css' 
-import 'primevue/resources/primevue.min.css'
 import 'primeicons/primeicons.css'
-
+// 修正后的正确路径：从 'lara' 文件夹内部导入 'light-blue'
+import Lara from '@primevue/themes/lara';
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
 import PrimeVue from 'primevue/config'
-
 import App from './App.vue'
 import router from './router'
 
@@ -20,6 +17,12 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-app.use(PrimeVue) // ✅ 挂载 PrimeVue
+
+// 使用我们用正确路径导入的 Preset 对象
+app.use(PrimeVue, {
+  theme: {
+    preset: Lara
+  }
+})
 
 app.mount('#app')
